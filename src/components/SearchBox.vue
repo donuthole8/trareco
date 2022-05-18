b692af9543c8c47d2a7d49ae4d02e96523f95b04
donuthole8 <muro5866@gmail.com>
2022-05-19 05:23:28 +0900

    feat:検索ボックス

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="main">
-
-
     <div class="search-box">
       <p>SearchBox called</p>
 
       <p>観光スポットや店を検索しよう！</p>
-      <form action="#" method="post">
-        <p>検索ワード：<br>
-        <input type="text" name="spot-name"></p>
-        <p><input type="submit" value="検索"></p>
-      </form>
+
+      <p>観光スポット・店名：<br>
+      <input type="text" v-model="keyword"></p>
+      <button @click="showSpots">検索</button>
+
+      <table>
+        <tr v-for="spot in filterdSpots" :key="spot.id">
+            <td v-text="spot.id"></td>
+            <td v-text="spot.name"></td>
+            <td>
+              <img src="spot.imageUrl" alt="">
+            </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -21,6 +28,34 @@ export default {
   name: 'SearchBox',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      keyword: "",
+      spots: [{
+        id: "",
+        name: "",
+        imageUrl: "",
+      }]
+    };
+  },
+  mothods: {
+    computed: {
+      // リアルタイム検索
+      filterdSpots: function() {
+        var spots = [];
+        for (var i in this.spots) {
+          var spot = this.spots[i];
+          if (spot.name.indexOf(this.keyword) !== -1) {
+            spots.push(spot)
+          }
+        }      
+      },
+      // スポット名の検索
+      showSpots: function() {
+
+      }
+    }
   }
 }
 </script>
