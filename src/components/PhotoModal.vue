fd07e99488c370b89e4c46a3bef9bc19a7161f6b
donuthole8 <muro5866@gmail.com>
2022-05-19 15:42:25 +0900

    feat:Firebase・DB動作確認

---
+++ src/components/PhotoModal.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="main">
+    <p>PhotoModal called</p>
+
+  <div>
+    <ModalButton :modalFlg="modalFlg" @modal-clicked="modalFlg = $event" />
+    <Main
+      :modalFlg="modalFlg"
+      @modal-clicked="modalFlg = $event"
+      :modalTtl="modalItems.modalTtl"
+      :modalContent="modalItems.modalContent"
+    />
+  </div>
+  
+  <!-- 
+    <div class="photo-modal">
+      <button class="btn modal">モーダル</button>
+
+      <button @click="showContent">思い出作成</button>
+
+      <div v-show="showContent" id="overlay">
+        <div id="modal">
+          <p>これがモーダルウィンドウです。</p>
+          <p><button @click="addPhoto">登録</button></p>
+          <p><button @click="closeModal">戻る</button></p>
+        </div>
+      </div> -->
+    <!-- </div> -->
+  </div>
+  
+</template>
+
+<script>
+import Main from './Main.vue';
+import ModalButton from './ModalButton.vue';
+
+export default {
+  name: 'PhotoModal',
+  components: {
+    Main,
+    ModalButton,
+  },
+  props: ["modalFlg"],
+  data() {
+    return {
+      childModalFlg: this.modalFlg,
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main {
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px dotted #333333;
+  border-radius: 5px;
+}
+#overlay {
+  /* 要素を重ねた時の順番 */
+  z-index: 1;
+  
+  /* 画面全体を覆う設定 */
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.5);
+
+  /* 画面の中央に要素を表示させる設定 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#modal {
+  z-index:2;
+  width:50%;
+  padding: 1em;
+  background:#fff;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2l-XCLSwRasJHT2235f3RK9MuMImGpF0",
+  authDomain: "trareco-ccb11.firebaseapp.com",
+  databaseURL: "https://trareco-ccb11-default-rtdb.firebaseio.com",
+  projectId: "trareco-ccb11",
+  storageBucket: "trareco-ccb11.appspot.com",
+  messagingSenderId: "102430406773",
+  appId: "1:102430406773:web:1decd3d90e7f3f9a9ee03c",
+  measurementId: "G-D3LV9CC9QW"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
